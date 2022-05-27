542e71244cb75c64ab10aea33d79eaa7a795b036
Manuel Nunes <manuel_s_nunes@yahoo.com>
2022-05-27 13:04:55 -0700

    changed http to https because of mixed protocol message

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const search = { search: async (query) =>
-  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`)
+  axios.get(`https://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`)
 };
 
 export default search;
